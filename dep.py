47db149e4ba2be98a4a5503fc6f10b9b77912398
LIAMSIIII <141633911+LIAMSIIII@users.noreply.github.com>
2023-08-07 12:34:43 +0100

    dep.py

--- dep.py
+++ dep.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open('model.pkl', 'rb'))
+loaded_model = pickle.load(open('C:/Users/arkou/Desktop/deployment/dep.py', 'rb'))
 
 
 # creating a function for Prediction
